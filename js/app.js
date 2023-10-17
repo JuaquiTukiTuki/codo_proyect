4a1ad4ec1c252601cdb79856fda8d57b3d51c45f
LAPTOP-4VAC9DJM\Joaqu <joaquinriffel.jr@gmail.com>
2023-10-17 19:14:36 -0300

    Update

--- js/app.js
+++ js/app.js
@@ -9,5 +9,10 @@ function mostrar_lista() {
     navbar_lista.classList.toggle('visible')
 }
 
+    function leer_scroll() {
+        
+}
+
 // Sección funcionalidades
 boton_lista_nav.addEventListener('click', mostrar_lista)
+window.addEventListener("scroll", leer_scroll)
